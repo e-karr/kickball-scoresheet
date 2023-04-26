de4e320779c4ec9362d0ca19454d124ef34aa187
Elizabeth Karr <e.karr93@icloud.com>
2023-04-26 18:42:37 -0500

    adjust score-container grid to better align gender, player name, offense, and defense columns

--- script.js
+++ script.js
@@ -9,28 +9,33 @@ for (let i = 0; i < 10; i++) {
 inningContainerColumn.innerHTML = inningColumnContent;
 
 let lineupOrder = document.getElementById("linup-order-container");
-let lineupContainer = document.getElementById("line-up-container");
+let genderColumn = document.getElementById('gender-col');
+
 let lineupOrderContent = "";
 let playerContent = "";
+let genderContent = "";
 
 for (let i = 0; i < 12; i++) {
     lineupOrderContent += `<p>${i+1}</p>`;
 
-    playerContent += `<div class="player-container">
-                        <input type="text" class="gender" name="gender">
-                        <input type="text" class="player-name" name="player-name">
-                        <input type="text" class="player2-name" name="player2-name">
-                        <div class="of-de">
-                            <input type="checkbox">
-                            <input type="checkbox">
-                            <input type="checkbox">
-                            <input type="checkbox">
-                        </div>
-                    </div>`;
+    genderContent += '<input type="text" class="gender" name="gender" placeholder=M/F/NB>'
+
+    // playerContent += `<div class="player-container">
+    //                     <input type="text" class="gender" name="gender">
+    //                     <input type="text" class="player-name" name="player-name">
+    //                     <input type="text" class="player2-name" name="player2-name">
+    //                     <div class="of-de">
+    //                         <input type="checkbox">
+    //                         <input type="checkbox">
+    //                         <input type="checkbox">
+    //                         <input type="checkbox">
+    //                     </div>
+    //                 </div>`;
 }
 
 lineupOrder.innerHTML = lineupOrderContent;
-lineupContainer.innerHTML = playerContent;
+
+genderColumn.innerHTML += genderContent;
 
 let scoresheetContainer = document.getElementById('scoresheet-container');
 let scoresheetDivs = "";
